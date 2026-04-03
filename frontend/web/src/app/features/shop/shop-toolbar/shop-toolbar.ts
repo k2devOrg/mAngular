b38b5ef2adbeb5d4c6833569b0a8878daa649da8
Konrad <konradk95@gmail.com>
2026-04-03 19:26:21 +0200

    dostosowanie filtrow UI

--- frontend/web/src/app/features/shop/shop-toolbar/shop-toolbar.ts
+++ frontend/web/src/app/features/shop/shop-toolbar/shop-toolbar.ts
@@ -9,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class ShopToolbarComponent {
 
+  categoryOpen = false;
+  priceOpen = false;
+  sortOpen = false;
+  selectedSortLabel = 'Sortowanie';
+
+  toggleCategory() {
+    this.categoryOpen = !this.categoryOpen;
+    this.priceOpen = false;
+    this.sortOpen = false;
+  }
+
+  togglePrice() {
+    this.priceOpen = !this.priceOpen;
+    this.categoryOpen = false;
+    this.sortOpen = false;
+  }
+
+
+  closeFilters() {
+    this.categoryOpen = false;
+    this.priceOpen = false;
+    this.sortOpen = false;
+  }
+
+
+  toggleSort() {
+    this.sortOpen = !this.sortOpen;
+    this.categoryOpen = false;
+    this.priceOpen = false;
+  }
+
+  selectSort(label: string) {
+    this.selectedSortLabel = label;
+    this.sortOpen = false;
+  }
+
 }
