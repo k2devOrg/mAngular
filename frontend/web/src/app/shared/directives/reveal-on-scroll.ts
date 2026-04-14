d4d53072c9d84ac2dbf2b29a6d59d0a42eb2733b
Konrad <konradk95@gmail.com>
2026-04-14 18:56:54 +0200

    wylaczenie directive reveal-on-scroll

--- frontend/web/src/app/shared/directives/reveal-on-scroll.ts
+++ frontend/web/src/app/shared/directives/reveal-on-scroll.ts
@@ -1,34 +1,46 @@
-import {Directive, ElementRef, HostBinding, signal, AfterViewInit, OnDestroy, } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appRevealOnScroll]',
   standalone: true,
 })
-export class RevealOnScroll implements AfterViewInit, OnDestroy {
-
-  private readonly isRevealed = signal<boolean>(false);
-  private observer: IntersectionObserver | null = null;
-
+export class RevealOnScroll {
   @HostBinding('class.is-revealed')
-  get isRevealedClass(): boolean {
-    return this.isRevealed();
-  }
-
-  constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
-
-  ngAfterViewInit(): void {
-    this.observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) {
-        this.isRevealed.set(true);
-        this.observer?.disconnect();
-      }
-    });
-
-    this.observer.observe(this.elementRef.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    this.observer?.disconnect();
-  }
-
+  readonly isRevealedClass = true;
 }
+
+
+// import {Directive, ElementRef, HostBinding, signal, AfterViewInit, OnDestroy, } from '@angular/core';
+//
+// @Directive({
+//   selector: '[appRevealOnScroll]',
+//   standalone: true,
+// })
+// export class RevealOnScroll implements AfterViewInit, OnDestroy {
+//
+//   private readonly isRevealed = signal<boolean>(false);
+//   private observer: IntersectionObserver | null = null;
+//
+//   @HostBinding('class.is-revealed')
+//   get isRevealedClass(): boolean {
+//     return this.isRevealed();
+//   }
+//
+//   constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
+//
+//   ngAfterViewInit(): void {
+//     this.observer = new IntersectionObserver(([entry]) => {
+//       if (entry.isIntersecting) {
+//         this.isRevealed.set(true);
+//         this.observer?.disconnect();
+//       }
+//     });
+//
+//     this.observer.observe(this.elementRef.nativeElement);
+//   }
+//
+//   ngOnDestroy(): void {
+//     this.observer?.disconnect();
+//   }
+//
+// }
